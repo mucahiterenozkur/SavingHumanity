be2954053b7611516b3b4adcc9639cc5a33352db
Mücahit Eren Özkur <mucahiterenozkur@hotmail.com>
2021-10-25 23:32:30 +0300

    timer added

--- Assets/Healthbar.cs
+++ Assets/Healthbar.cs
@@ -2,24 +2,64 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class Healthbar : MonoBehaviour
 {
+    private MeteorSpawner meteorSpawner;
+
     public Slider slider;
     public Image fillArea;
-    private int totalHealth = 10;
+    private int totalHealth = 20;
+
+    public TextMeshProUGUI timerText;
+    public float timeRemaining = 120;
+    public static bool timerIsRunning = false;
+
+    public GameObject inGameCanvas;
+    public GameObject gameOverCanvas;
 
     // Start is called before the first frame update
     void Start()
     {
+        meteorSpawner = FindObjectOfType<MeteorSpawner>();
+
+        timerIsRunning = true;
         slider.value = totalHealth;
         fillArea.color = Color.green;
+
+        inGameCanvas.SetActive(true);
+        gameOverCanvas.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (timerIsRunning)
+        {
+            if (timeRemaining > 0)
+            {
+                timeRemaining -= Time.deltaTime;
+                DisplayTime(timeRemaining);
+            }
+            else
+            {
+                gameOverCanvas.SetActive(true);
+                Debug.Log("Time has run out!");
+                timeRemaining = 0;
+                timerIsRunning = false;
+            }
+        }
+    }
+
+    public void DisplayTime(float timeToDisplay)
+    {
+        timeToDisplay += 1;
+
+        float minutes = Mathf.FloorToInt(timeToDisplay / 60);
+        float seconds = Mathf.FloorToInt(timeToDisplay % 60);
+
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
     }
 
     private void OnCollisionEnter2D(Collision2D other)
--- Assets/Scripts/MeteorSpawner.cs
+++ Assets/Scripts/MeteorSpawner.cs
@@ -26,30 +26,39 @@ public class MeteorSpawner : MonoBehaviour
         //}
     }
 
-    IEnumerator SpawnMeteor()
+    public IEnumerator SpawnMeteor()
     {
         //Debug.Log("co worked");
-        List<int> whereToPlaced = new List<int>();
 
-        for (int i = 0; i < 10; i++)
+        if (Healthbar.timerIsRunning)
         {
-            int randomPoint = Random.Range(0, 7);
+            List<int> whereToPlaced = new List<int>();
 
-            if (!whereToPlaced.Contains(randomPoint))
+            for (int i = 0; i < 10; i++)
             {
-                whereToPlaced.Add(randomPoint);
-                //Debug.Log(randomPoint);
-                int randomMeteor = Random.Range(0, meteors.Count);
-                GameObject meteor = Instantiate(meteors[randomMeteor], spawnPoints[randomPoint].position, spawnPoints[randomPoint].rotation, spawnPoints[randomPoint].transform) as GameObject;
-                meteor.GetComponent<Rigidbody2D>().velocity = Vector2.left * meteorSpeed; // dont put time.deltatime here because it affects the velocity of meteors.
-                //Debug.Log(meteor.GetComponent<Rigidbody2D>().velocity);
-            }
+                int randomPoint = Random.Range(0, 7);
 
+                if (!whereToPlaced.Contains(randomPoint))
+                {
+                    whereToPlaced.Add(randomPoint);
+                    //Debug.Log(randomPoint);
+                    int randomMeteor = Random.Range(0, meteors.Count);
+                    GameObject meteor = Instantiate(meteors[randomMeteor], spawnPoints[randomPoint].position, spawnPoints[randomPoint].rotation, spawnPoints[randomPoint].transform) as GameObject;
+                    meteor.GetComponent<Rigidbody2D>().velocity = Vector2.left * meteorSpeed; // dont put time.deltatime here because it affects the velocity of meteors.
+                    //Debug.Log(meteor.GetComponent<Rigidbody2D>().velocity);
+                }
+
+            }
         }
+        
 
         yield return new WaitForSeconds(2f);
 
-        StartCoroutine(SpawnMeteor());
+        if (Healthbar.timerIsRunning)
+        {
+            StartCoroutine(SpawnMeteor());
+        }
+        
 
     }
 
